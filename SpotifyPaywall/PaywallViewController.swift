33a42f59fa13ad22685e7e87a85318a81de5aab6
최지연 <choi02647@naver.com>
2023-08-01 17:27:27 +0900

    fix ui

--- SpotifyPaywall/PaywallViewController.swift
+++ SpotifyPaywall/PaywallViewController.swift
@@ -9,4 +9,49 @@ import UIKit
 
 class PaywallViewController: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    let bannerInfos: [BannerInfo] = BannerInfo.list
+    let colors: [UIColor] = [.systemPurple, .systemOrange, .systemPink, .systemRed]
+    
+    enum Section {
+        case main
+    }
+    var datasource: UICollectionViewDiffableDataSource<Section, BannerInfo>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        datasource = UICollectionViewDiffableDataSource<Section, BannerInfo>.init(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as? BannerCell else {
+                return nil
+            }
+            cell.configure(item)
+            cell.backgroundColor = self.colors[indexPath.item]
+            return cell
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, BannerInfo>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(bannerInfos, toSection: .main)
+        datasource.apply(snapshot)
+        
+        collectionView.collectionViewLayout = layout()
+        
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
 }
