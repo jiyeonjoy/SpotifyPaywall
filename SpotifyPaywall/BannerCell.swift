0f20fcd984b8e4bc4ba02f32900d6272a3242da2
최지연 <choi02647@naver.com>
2023-08-01 17:06:04 +0900

    done ui

--- SpotifyPaywall/BannerCell.swift
+++ SpotifyPaywall/BannerCell.swift
@@ -5,4 +5,22 @@
 //  Created by 최지연/클라이언트 on 2023/08/01.
 //
 
-import Foundation
+import UIKit
+
+class BannerCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.cornerRadius = 16
+    }
+    
+    func configure(_ info: BannerInfo) {
+        thumbnailImageView.image = UIImage(named: info.imageName)
+        titleLabel.text = info.title
+        descriptionLabel.text = info.description
+    }
+}
