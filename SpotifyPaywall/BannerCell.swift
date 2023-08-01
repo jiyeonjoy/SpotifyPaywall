0e6aa7bf9668aded7b187869801db76e22e8501e
최지연 <choi02647@naver.com>
2023-08-01 16:04:47 +0900

    added banner info

---
+++ SpotifyPaywall/BannerCell.swift
@@ -0,0 +1,8 @@
+//
+//  BannerCell.swift
+//  SpotifyPaywall
+//
+//  Created by 최지연/클라이언트 on 2023/08/01.
+//
+
+import Foundation
---
+++ SpotifyPaywall/PaywallViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PaywallViewController.swift
+//  SpotifyPaywall
+//
+//  Created by 최지연/클라이언트 on 2023/08/01.
+//
+
+import UIKit
+
+class PaywallViewController: UIViewController {
+    
+}
